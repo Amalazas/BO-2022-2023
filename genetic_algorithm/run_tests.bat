e8ef6c23f6b05115133367a83b678473a6d92b59
Jan Prokop <josidek@gmail.com>
2023-06-06 15:11:47 +0200

    Example test batch file tweak

--- genetic_algorithm/run_tests.bat
+++ genetic_algorithm/run_tests.bat
@@ -1,6 +1,6 @@
-py tester.py test_input_1.txt 1500 400 2 0.01 random add_packs 0.2 10 10
-py tester.py test_input_1.txt 1500 400 2 0.01 random inverse_packages 0.2 10 10
-py tester.py test_input_1.txt 1500 400 2 0.01 random cut_out_packs 0.2 10 10
-py tester.py test_input_1.txt 1500 400 2 0.01 random inverse_permutation 0.2 10 10
-py tester.py test_input_1.txt 1500 400 2 0.01 random shift_block 0.2 10 10
-py tester.py test_input_1.txt 1500 400 2 0.01 random shuffle_block 0.2 10 10
+py tester.py test_input_1.txt 1500 400 2 70 0.01 0.3 random add_packs 0.2 10 10
+py tester.py test_input_1.txt 1500 400 2 70 0.01 0.3 random inverse_packages 0.2 10 10
+py tester.py test_input_1.txt 1500 400 2 70 0.01 0.3 random cut_out_packs 0.2 10 10
+py tester.py test_input_1.txt 1500 400 2 70 0.01 0.3 random inverse_permutation 0.2 10 10
+py tester.py test_input_1.txt 1500 400 2 70 0.01 0.3 random shift_block 0.2 10 10
+py tester.py test_input_1.txt 1500 400 2 70 0.01 0.3 random shuffle_block 0.2 10 10
